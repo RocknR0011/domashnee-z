2253be0a0ebe5ec7fbb38d747722a9fe3b48a4b4
RocknR0011 <95881702+RocknR0011@users.noreply.github.com>
2022-03-21 15:10:33 +0300

    создалMAX из 3х чисел

---
+++ 3/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = 15;
+int numberB = 25;
+int numberC = 46;
+int max = numberA;
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+Console.WriteLine(max);

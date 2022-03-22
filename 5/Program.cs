872702c26ffbc2bd7606a8eef5e36628c880d196
RocknR0011 <95881702+RocknR0011@users.noreply.github.com>
2022-03-22 14:26:22 +0300

    создалчисла и день недели

---
+++ 5/Program.cs
@@ -0,0 +1,18 @@
+﻿int number1 = new int();
+System.Console.WriteLine("Введите число от 1 до 7 : ");
+number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 == 1)
+{ Console.WriteLine("понедельник"); }
+if (number1 == 2)
+{ Console.WriteLine("вторник"); }
+if (number1 == 3)
+{ Console.WriteLine("среда"); }
+if (number1 == 4)
+{ Console.WriteLine("четверг"); }
+if (number1 == 5)
+{ Console.WriteLine("пятница"); }
+if (number1 ==6)
+{ Console.WriteLine("суббота"); }
+if (number1 == 7)
+{ Console.WriteLine("воскресенье"); }
+

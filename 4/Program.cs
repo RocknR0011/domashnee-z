99a15ee6fcb2452b875c950c5cb3c6749c0043ff
RocknR0011 <95881702+RocknR0011@users.noreply.github.com>
2022-03-22 14:15:41 +0300

    создалпроверку из 2х чисел

---
+++ 4/Program.cs
@@ -0,0 +1,15 @@
+﻿int number1 = new int();
+System.Console.WriteLine("Введите число 1 : ");
+number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = new int();
+System.Console.WriteLine("Введите число 2 : ");
+number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 / number1 == number1)
+{ Console.WriteLine("да"); }
+
+if (number2 / number1 < number1)
+{ Console.WriteLine("нет"); }
+
+if (number2 / number1 > number1)
+{ Console.WriteLine("нет"); }

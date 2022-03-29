52e6fc05355640bacea985ee670efb64eef960f8
RocknR0011 <95881702+RocknR0011@users.noreply.github.com>
2022-03-29 15:57:29 +0300

    создал дз к 2 семинар.у

---
+++ 8семинар2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+int number = new int();
+System.Console.WriteLine("Введите 3ех значное число ");
+number = Convert.ToInt32(Console.ReadLine());
+number = number / 10 ;
+int number1 = number % 10;
+Console.WriteLine(number1);

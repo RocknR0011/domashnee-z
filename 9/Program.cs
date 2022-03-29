eccf880b200b21c201ebf0a68ab30b4b90f5343f
RocknR0011 <95881702+RocknR0011@users.noreply.github.com>
2022-03-29 16:04:47 +0300

    создал дз к 2 семинару задача2

--- 9/Program.cs
+++ 9/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int number1 = new int();
+System.Console.WriteLine("Введите число от 1 до 7 : ");
+number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 == 1)
+{ Console.WriteLine("понедельник"); }
+if (number1 == 2)
+{ Console.WriteLine("вторник"); }
+if (number1 == 3)
+{ Console.WriteLine("среда"); }
+if (number1 == 4)
+{ Console.WriteLine("четверг"); }
+if (number1 == 5)
+{ Console.WriteLine("пятница"); }
+if (number1 ==6)
+{ Console.WriteLine("суббота-выходной"); }
+if (number1 == 7)
+{ Console.WriteLine("воскресенье-выходной"); }
+
